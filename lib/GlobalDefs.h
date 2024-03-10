bde3d2c7e864fce9f8b63e7bafb9729b94ea6c4e
Leviathan <levi.mcgomes@gmail.com>
2024-03-10 18:17:33 +0000

    Inlined GlobalDefs function definitions.

--- lib/GlobalDefs.h
+++ lib/GlobalDefs.h
@@ -3,9 +3,17 @@
 
 typedef size_t (*PrintLog)(const char *);
 
-size_t Serial_println(const char *line);
+inline size_t Serial_println(const char *line)
+{
+    return Serial.println(line);
+}
 
 template <typename... Args>
-const char *sprintf_get(const char *format, Args... args);
+const char *sprintf_get(const char *format, Args... args)
+{
+    char *result;
+    sprintf(result, format, args...);
+    return result;
+}
 
 #endif
