ea941433b1558d8ce18440ba0d6b846579e98266
Leviathan <levi.mcgomes@gmail.com>
2024-03-10 17:58:14 +0000

    Added a (very barebones) DI-ish logging workflow on GlobalDefs.

--- lib/GlobalDefs.h
+++ lib/GlobalDefs.h
@@ -1,6 +1,10 @@
 #ifndef INCL_GLOBAL_DEFS
 #define INCL_GLOBAL_DEFS
 
+typedef size_t (*PrintLog)(const char *);
+
+size_t Serial_println(const char *line);
+
 template <typename... Args>
 const char *sprintf_get(const char *format, Args... args);
 
--- src/GlobalDefs.cpp
+++ src/GlobalDefs.cpp
@@ -1,5 +1,9 @@
 #include <GlobalDefs.h>
 
+size_t Serial_println(const char *line)
+{
+    return Serial.println(line);
+}
 
 template <typename... Args>
 const char *sprintf_get(const char *format, Args... args)
