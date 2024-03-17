bc1857b32edc33b4c89c6832c52a0a935d108d91
Leviathan <levi.mcgomes@gmail.com>
2024-03-17 14:36:43 +0000

    Testing code for PressureTemperature.

--- cansat-code24.ino
+++ cansat-code24.ino
@@ -1,4 +1,5 @@
 #include <PinDefinitions.h>
+#include <GlobalDefs.h>
 #include <PressureTemperature.h>
 #include <Accelerometer.h>
 #include <GPS.h>
@@ -6,10 +7,20 @@
 #include <RadioCommunication.h>
 #include <SDManagement.h>
 
+PressureTemperature pressureTemp(Serial_println);
+
 void setup()
 {
+  Serial.begin(115200);
+  pressureTemp.Setup();
+  Serial.flush();
 }
 
 void loop()
 {
+  float t = pressureTemp.GetTemperature(), p = pressureTemp.GetPressure();
+  Serial.println((String)"Temperature = "+t+" ºC");
+  Serial.println((String)"Pressure = "+p+" Pa");
+  Serial.println((String)"Altitude = "+pressureTemp.GetAltitude(t, p)+" m");
+  delay(5000);
 }
